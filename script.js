fb4a9e3e13f720f3ca7a2868592842504f82bbf8
dpxn23 <davidxuan.nguyen01@gmail.com>
2021-03-30 01:49:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).on("load", start);
+
+function start(){
+
+    ScrollReveal().reveal('.reveal1');
+    ScrollReveal().reveal('.reveal2', { delay: 500 });
+    ScrollReveal().reveal('.reveal3', { delay: 800 });
+}
